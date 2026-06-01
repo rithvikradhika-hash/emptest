7954474ff185f8033745377fb6108e34242c6e55
rithvikradhika-hash <rithvikradhika@gmail.com>
2026-06-01 11:53:58 +0530

    created

---
+++ emp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return(
+    <>
+    <h1 className='text-white font-sans'>hi</h1>
+    </>
+  )
+}
+
+export default App
